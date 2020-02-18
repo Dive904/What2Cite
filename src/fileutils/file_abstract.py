154eddd6f67de3541dfeeac7d350c141545f0731
Davide G <giosa.davide@gmail.com>
2020-02-18 19:30:33 +0100

    abstract reader

--- src/fileutils/file_abstract.py
+++ src/fileutils/file_abstract.py
@@ -17,3 +17,30 @@ def txt_abstract_creator(filename, list_dic):
             f.write("*** TITLE: " + d["title"] + "\n")
             f.write("*** ABSTRACT: " + d["paperAbstract"] + "\n")
             f.write("---" + "\n")
+
+
+def txt_only_abstract_reader(filepath):
+    result = []
+    d = {}
+
+    state = "e"
+    with io.open(filepath, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("*** TITLE: "):
+                state = "t"
+                d["title"] = line.split("*** TITLE: ")[1][:-1]
+            elif line.startswith("*** ABSTRACT: "):
+                state = "a"
+                d["paperAbstract"] = line.split("*** ABSTRACT: ")[1][:-1]
+            elif line.startswith("---"):
+                result.append(d)
+                state = "e"
+                d = {}
+            else:
+                if state == "t":
+                    d["title"] += line[:-1]
+                elif state == "a":
+                    d["paperAbstract"] += line[:-1]
+
+    return result
