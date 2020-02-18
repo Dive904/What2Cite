d1a4b4884824a15119e308400c4955f090c8af39
Davide G <giosa.davide@gmail.com>
2020-02-18 16:18:52 +0100

    added comments

--- src/fileutils/file_abstract.py
+++ src/fileutils/file_abstract.py
@@ -1,7 +1,17 @@
+# Library with file utils
+
 import io
 
 
 def txt_abstract_creator(filename, list_dic):
+    """
+    Use this method to create a txt file with paper:
+    TITLE
+    ABSTRACT
+    ---
+    :param filename: filename
+    :param list_dic: list of json dictionary (or a dictionary with title and abstract)
+    """
     with io.open(filename + ".txt", "w", encoding="utf-8") as f:
         for d in list_dic:
             f.write("*** TITLE: " + d["title"] + "\n")
