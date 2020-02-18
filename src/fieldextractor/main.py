14a42ced19567ea8981fd336fe4f115f73eaebc9
Davide G <giosa.davide@gmail.com>
2020-02-18 12:32:20 +0100

    ready to extract all

--- src/fieldextractor/main.py
+++ src/fieldextractor/main.py
@@ -12,29 +12,29 @@ fos = "Computer Science"
 dir_path = "D:\\Università\\Corsi\\Tesi\\SemanticScolarDataset"
 
 files = os.listdir(dir_path)[2:-1]
-inputfile = files[0]
-print("INFO: Switching to archive " + inputfile)
+for inputfile in files:
+    print("INFO: Switching to archive " + inputfile)
 
-file_path = "D:\\Università\\Corsi\\Tesi\\SemanticScolarDataset\\" + inputfile
-filecontent_name = "../../output/txtabstract/" + inputfile[:-3] + "_extracted"
+    file_path = "D:\\Università\\Corsi\\Tesi\\SemanticScolarDataset\\" + inputfile
+    filecontent_name = "../../output/txtabstract/" + inputfile[:-3] + "_extracted"
 
-print("INFO: Extracting archive", end="... ")
-with gzip.open(file_path, 'rb') as f_in:
-    with open(filecontent_name, 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
-print("Done ✓")
+    print("INFO: Extracting archive", end="... ")
+    with gzip.open(file_path, 'rb') as f_in:
+        with open(filecontent_name, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    print("Done ✓")
 
-print("INFO: Loading JSON and filtering", end="... ")
-result_dic = []
-for line in open(filecontent_name, 'r', encoding="utf-8"):
-    d = json.loads(line)
-    if utils.apply_filter(d):
-        result_dic.append(d)
+    print("INFO: Loading JSON and filtering", end="... ")
+    result_dic = []
+    for line in open(filecontent_name, 'r', encoding="utf-8"):
+        d = json.loads(line)
+        if utils.apply_filter(d):
+            result_dic.append(d)
 
-print("Done ✓")
+    print("Done ✓")
 
-print("INFO: Writing output file", end="... ")
-file_abstract_creator.txt_abstract_creator(filecontent_name, result_dic)
-print("Done ✓")
+    print("INFO: Writing output file", end="... ")
+    file_abstract_creator.txt_abstract_creator(filecontent_name, result_dic)
+    print("Done ✓")
 
-os.remove(filecontent_name)
+    os.remove(filecontent_name)
