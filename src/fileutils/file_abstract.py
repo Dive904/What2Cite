cf127dd9204da4e9d57acbad758217818332d6f8
Davide G <giosa.davide@gmail.com>
2020-02-20 12:25:41 +0100

    other improvements

--- src/fileutils/file_abstract.py
+++ src/fileutils/file_abstract.py
@@ -20,7 +20,7 @@ def txt_abstract_creator(filename, list_dic):
             f.write("---" + "\n")
 
 
-def txt_abstract_reader(filepath):
+def txt_dataset_reader(filepath):
     """
     Use this method to read an abstract file
     :param filepath: filepath
@@ -28,25 +28,18 @@ def txt_abstract_reader(filepath):
     """
     result = []
     d = {}
-    state = "e"
     with io.open(filepath, "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
-            if line.startswith("*** TITLE: "):
-                state = "t"
+            if line.startswith("*** ID: "):
+                d["id"] = line.split("*** ID: ")[1][:-1]
+            elif line.startswith("*** TITLE: "):
                 d["title"] = line.split("*** TITLE: ")[1][:-1]
             elif line.startswith("*** ABSTRACT: "):
-                state = "a"
                 d["paperAbstract"] = line.split("*** ABSTRACT: ")[1][:-1]
             elif line.startswith("---"):
                 result.append(d)
-                state = "e"
                 d = {}
-            else:
-                if state == "t":
-                    d["title"] += line[:-1]
-                elif state == "a":
-                    d["paperAbstract"] += line[:-1]
 
     return result
 
@@ -58,23 +51,10 @@ def txt_only_abstract_reader(filepath):
     :return: a list of abstract
     """
     result = []
-    state = "e"
-    tmp_abstract = ""
     with io.open(filepath, "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
-            if not line.startswith(" \n"):
-                if line.startswith("*** TITLE: "):
-                    state = "t"
-                elif line.startswith("*** ABSTRACT: "):
-                    state = "a"
-                    tmp_abstract = line.split("*** ABSTRACT: ")[1][:-1]
-                elif line.startswith("---"):
-                    result.append(tmp_abstract)
-                    state = "e"
-                    tmp_abstract = ""
-                else:
-                    if state == "a":
-                        tmp_abstract += " " + line[:-1]
+            if line.startswith("*** ABSTRACT: "):
+                result.append(line.split("*** ABSTRACT: ")[1][:-1])
 
     return result
