e5f078badefdc12ecd78245138325d659d8161c1
Davide G <giosa.davide@gmail.com>
2020-02-27 14:00:24 +0100

    citations lda

---
+++ src/topicmodeller/lda_method_outcitations.py
@@ -0,0 +1,32 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation as LDA
+
+from src.topicmodeller import tm_utils
+
+out_filename = "../../output/citations/topics_cits.txt"
+batch_number = 65
+number_topics = 55
+number_words = 5
+
+print("INFO: Extracting citations", end="... ")
+citations_extracted = tm_utils.extract_only_citations("C:\\Users\\Davide\\Desktop\\semanticdatasetextracted\\",
+                                                      end=batch_number)
+print("Done ✓")
+
+# Initialise the count vectorizer with the English stop words
+count_vectorizer = CountVectorizer(max_df=0.8, min_df=2, stop_words='english')
+
+# Fit and transform the processed titles
+print("INFO: Fitting count vectorizer", end="... ")
+count_data = count_vectorizer.fit_transform(citations_extracted)
+print("Done ✓")
+
+# Create and fit the LDA model
+print("INFO: Computing LDA", end="... ")
+lda = LDA(n_components=number_topics, n_jobs=-1)
+lda.fit(count_data)  # Print the topics found by the LDA model
+print("Done ✓")
+
+print("INFO: Writing topics on file", end="... ")
+tm_utils.print_topics_in_file(lda, count_vectorizer, number_words, out_filename)
+print("Done ✓")
