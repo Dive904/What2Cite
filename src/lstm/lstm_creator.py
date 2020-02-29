bc983e33538d258fc9cb57b45ff36d4cc989a875
Davide G <giosa.davide@gmail.com>
2020-02-29 11:59:33 +0100

    tokenizing

--- src/lstm/lstm_creator.py
+++ src/lstm/lstm_creator.py
@@ -4,7 +4,9 @@ import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from nltk.corpus import stopwords
-STOPWORDS = set(stopwords.words('english'))
+
+additional_stopwords = ["paper", "method", "large", "model", "proposed", "study", "based", "using", "approach", "also"]
+STOPWORDS = set(stopwords.words('english')).union(set(additional_stopwords))
 
 print(tf.__version__)
 vocab_size = 5000
@@ -23,7 +25,7 @@ with open("../../output/lstmdataset/data_reduced.csv", encoding="utf-8") as csvf
     next(reader)
     for row in reader:
         labels.append(row[1])
-        article = row[0]
+        article = row[0].lower()
         for word in STOPWORDS:
             token = ' ' + word + ' '
             article = article.replace(token, ' ')
@@ -32,3 +34,25 @@ with open("../../output/lstmdataset/data_reduced.csv", encoding="utf-8") as csvf
 
 print(len(labels))
 print(len(articles))
+
+train_size = int(len(articles) * training_portion)
+
+train_articles = articles[0: train_size]
+train_labels = labels[0: train_size]
+
+validation_articles = articles[train_size:]
+validation_labels = labels[train_size:]
+
+print(train_size)
+print(len(train_articles))
+print(len(train_labels))
+print(len(validation_articles))
+print(len(validation_labels))
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(train_articles)
+word_index = tokenizer.word_index
+print(dict(list(word_index.items())[0:10]))
+
+train_sequences = tokenizer.texts_to_sequences(train_articles)
+print(train_sequences[10])
