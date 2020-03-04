5e288a9c41ad68076bedae4c7132c8b5a800337f
Davide G <giosa.davide@gmail.com>
2020-03-04 18:59:29 +0100

    new stats

--- src/stats/text_number.py
+++ src/stats/text_number.py
@@ -3,17 +3,12 @@
 import os
 import io
 
-dir_path = "C:\\Users\\Davide\\Desktop\\TesiAPPimproved\\"
-files = os.listdir(dir_path)
+path = "../../output/lstmdataset/final.txt"
 total_count = 0
 
-for file in files:
-    print("INFO: Counting text from " + file, end="... \n")
-    tmp_count = 0
-    with io.open(dir_path + file, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        for line in lines:
-            if line.startswith("---"):
-                tmp_count += 1
-    total_count += tmp_count
-    print("INFO: Text in file: " + str(tmp_count) + ". Total count: " + str(total_count), end="\n\n")
+with io.open(path, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith("---"):
+            total_count += 1
+print("INFO: Total count: " + str(total_count), end="\n\n")
