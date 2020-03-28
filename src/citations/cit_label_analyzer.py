7b4878702fbcd5f39ebf24a76a83b9e590488329
Davide G <giosa.davide@gmail.com>
2020-03-28 16:25:12 +0100

    saving list to use in pipeline

--- src/citations/cit_label_analyzer.py
+++ src/citations/cit_label_analyzer.py
@@ -5,6 +5,7 @@ import numpy as np
 
 cit_labelled_path = "../../output/official/topics_cits_labelled_pickle.pickle"
 cit_labelled_analyzed_path = "../../output/official/cit_labelled_with_final_topic.txt"
+cit_labelled_analyzed_pickle_path = "../../output/official/cit_labelled_with_final_topic_pickle.pickle"
 
 w = [1, 0.75, 0.50]  # score of every position
 
@@ -29,6 +30,7 @@ print("Done ✓")
 
 first_step_result = list(map(lambda x: np.round(x, 2), first_step_result))  # rounding scores
 second_step_result = []
+list_to_pickle = []
 
 # this part is used to get the maximal topic for a CitTopic, maybe this will not necessary anymore...
 for l in first_step_result:
@@ -39,7 +41,11 @@ for l in first_step_result:
             pos.append(i)
 
     second_step_result.append((list(l), pos))
+    list_to_pickle.append(list(l))
 
 with open(cit_labelled_analyzed_path, "w") as out_file:  # writing results on disk
     for i in range(len(second_step_result)):
         out_file.write(str(i) + " -> " + str(second_step_result[i][0]) + " -> " + str(second_step_result[i][1]) + "\n")
+
+with open(cit_labelled_analyzed_pickle_path, "wb") as handle_file:  # saving list to use in pipeline application
+    pickle.dump(list_to_pickle, handle_file, protocol=pickle.HIGHEST_PROTOCOL)
