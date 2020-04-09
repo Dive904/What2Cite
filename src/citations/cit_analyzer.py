d2653b41e4f5f3873244bb2008a1cdf52b4226b9
Davide G <giosa.davide@gmail.com>
2020-04-09 12:44:51 +0200

    slow

--- src/citations/cit_analyzer.py
+++ src/citations/cit_analyzer.py
@@ -1,83 +1,37 @@
 # This script is used to analyze the CitTopic, classifying all the papers in the CitTopic and getting the TopK
-
 import pickle
 import gc
-import numpy as np
 
 from keras.engine.saving import load_model
 from keras_preprocessing.sequence import pad_sequences
 
-from src.lstm import lstm_utils
+from src.fileutils import file_abstract
 from src.citations import cit_utils
-from src.topicmodeller import tm_utils
+from src.lstm import lstm_utils
+from src.pipelineapplication import utils
 
 # input
-batch_number = 90
-k = 3
+close_dataset = "../../output/closedataset/closedataset.txt"
 topic_citations_filename = "../../output/official/topics_cits.txt"
-topic_filename = "../../output/official/topics.txt"
-abstract_document_topic_filename = "../../output/official/abstract_document_topic.csv"
 lstm_model = "../../output/official/lstm.h5"
 tokenizer_model = "../../output/official/tokenizer.pickle"
-cit_topic_info_pickle_path = "../../output/official/cit_topic_info_pickle.pickle"
+topic_number = 40
+t_for_true_prediction = 0.4  # probability threshold to consider a prediction as valid
 
 # output
-topics_cits_labelled_filename = "../../output/official/topics_cits_labelled.txt"
-topics_cits_labelled_pickle_filename = "../../output/official/topics_cits_labelled_pickle.pickle"
+cit_structure_pickle_path = "../../output/closedataset/cit_structure_pickle.pickle"
 
-print("INFO: Reading Abstract-Document-Topic Matrix", end="... ")
-abstract_document_topic_matrix = cit_utils.get_abstract_document_topic_matrix(abstract_document_topic_filename)
-print("Done ✓")
-
-print("INFO: Reading Citation Topic file", end="... ")
+print("INFO: Reading Citation Topic file and initializing structure", end="... ")
+cit_structure = {}
 cit_topic = cit_utils.read_topics_cit_file(topic_citations_filename)
-t = []
-for c in cit_topic:
-    tmp = []
-    for x in c:
-        tmp.append((x, None, 0))
-    t.append(tmp)
-cit_topic = t
+for cit in cit_topic:
+    for c in cit:
+        cit_structure[c] = [0 for x in range(topic_number)]
 print("Done ✓")
 
-# in this part of code, we scan the CitTopics looking for classified Topic in the LDA (from the abstract document
-# topic matrix)
-first_step_result = []
-for c in cit_topic:
-    tmp = []
-    for x in c:
-        paper_id = x[0]
-        paper_list = abstract_document_topic_matrix.get(paper_id)
-        if paper_list is not None:
-            topK_index, topK_elem = tm_utils.find_n_maximum(paper_list, k)
-            second = list(zip(topK_index, topK_elem))
-            third = 1
-        else:
-            second = None
-            third = 0
-        tmp.append((paper_id, second, third))
-    first_step_result.append(tmp)
-
-for t in first_step_result:
-    print(t)
-print()
-
-print()
-count = cit_utils.count_none(first_step_result)
-print("******************* None Count: " + str(count))
-print()
-
-print("INFO: Cleaning memory", end="... ")
-abstract_document_topic_matrix = None
-cit_topic = None
-t = None
-gc.collect()
+print("INFO: Reading close dataset", end="... ")
+paper_info = file_abstract.txt_dataset_reader(close_dataset)
 print("Done ✓")
-
-# from the first scan, it's probably that we have some elements without classified topic. Now we look for other
-# information using the rest of dataset
-with open(cit_topic_info_pickle_path, 'rb') as handle:  # take the list of CitTopic score
-    cit_topic_info = pickle.load(handle)
 
 print("INFO: Reading Tokenizer and Neural Network", end="... ")
 with open(tokenizer_model, 'rb') as handle:
@@ -86,46 +40,28 @@ with open(tokenizer_model, 'rb') as handle:
 model = load_model(lstm_model)
 print("Done ✓")
 
-print("INFO: Classifing None papers", end="... ")
-# with this type of loop, for optimal reasons, we scan the dataset only once
-for i in range(len(first_step_result)):
-    for j in range(len(first_step_result[i])):
-        paper_id = first_step_result[i][j][0]
-        topic = first_step_result[i][j][1]
-        third = first_step_result[i][j][2]
-        if topic is None:
-            couple = cit_topic_info.get(paper_id)
-            if (couple is not None) and (couple[1] is not None):
-                text = couple[1]
+print("INFO: Analyzing", end="... ")
+for paper in paper_info:
+    out_citations = paper["outCitations"]
+    topic = None
+    for out_cit in out_citations:
+        score_topic = cit_structure.get(out_cit)
+        if score_topic is not None:
+            if topic is None:
+                text = paper["paperAbstract"]
                 text = lstm_utils.preprocess_text(text)
                 seq = tokenizer.texts_to_sequences([text])
                 seq = pad_sequences(seq, padding='post', maxlen=200)
-                yhat = model.predict(seq)
-                yhat = list(yhat[0])
-                topK_index, topK_elem = tm_utils.find_n_maximum(yhat, k)
-                topK_elem = list(map(lambda y: np.round(y, 3), topK_elem))
-                topic = list(zip(topK_index, topK_elem))
-                third = 2
-                first_step_result[i][j] = (paper_id, topic, third)
+                topic = model.predict(seq)
+                valid_topic = utils.get_valid_predictions(topic[0], t_for_true_prediction)
+                topic = list(map(lambda x: x[0], valid_topic))
+            for t in topic:
+                score_topic[t] += 1
+            cit_structure[out_cit] = score_topic
+    gc.collect()
 print("Done ✓")
-second_step_result = first_step_result
 
-print()
-for t in second_step_result:
-    print(t)
-print()
-count = cit_utils.count_none(second_step_result)
-print("******************* None Count: " + str(count))
-print()
-
-new_cit_topic = None
-gc.collect()
-
-print("INFO: Writing output file", end="... ")
-with open(topics_cits_labelled_filename, "w") as output_file:
-    for i in range(len(second_step_result)):
-        output_file.write(str(i) + " -> " + str(second_step_result[i]) + "\n")
-
-with open(topics_cits_labelled_pickle_filename, "wb") as handle_file:
-    pickle.dump(second_step_result, handle_file, protocol=pickle.HIGHEST_PROTOCOL)
+print("INFO: Writing on file", end="... ")
+with open(cit_structure_pickle_path, "wb") as handle_file:  # saving list to use in pipeline application
+    pickle.dump(cit_structure, handle_file, protocol=pickle.HIGHEST_PROTOCOL)
 print("Done ✓")
